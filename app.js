483a80b1a4327f61df38f80183481b477e30158f
Alexander-Eryomenko <sanyochek1991@gmail.com>
2020-09-01 20:08:46 +0300

    strelka

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const loadForm = async () {
+const loadForm = async () => {
   const html = await (await fetch('https://alexander-eryomenko.github.io/index/')).json()
   document.body.appendChild(document.createElement('main')).innerHTML = html
 }
