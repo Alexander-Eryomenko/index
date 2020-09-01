b1133119265e7797183edc52d76003b5f344c74d
Alexander-Eryomenko <sanyochek1991@gmail.com>
2020-09-01 20:06:09 +0300

    create async

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-const img = document.body.appendChild(document.createElement('img'))
-img.src = 'https://unews.pro/insert/20200901/Unews202009011598969567_923362532.jpg'
+const loadForm = async () {
+  const html = await (await fetch('https://alexander-eryomenko.github.io/index/')).json()
+  document.body.appendChild(document.createElement('main')).innerHTML = html
+}
+loadForm()
