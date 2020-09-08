85f7ca09514b0500dae918999e24935c71af4434
Alexander-Eryomenko <sanyochek1991@gmail.com>
2020-09-08 20:45:02 +0300

    finish

--- app.js
+++
@@ -1,5 +0,0 @@
-const loadForm = async () => {
-  const html = await (await fetch('https://alexander-eryomenko.github.io/index/')).json()
-  document.body.appendChild(document.createElement('main')).innerHTML = html
-}
-loadForm()
